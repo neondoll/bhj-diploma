32763f7c6ea5f2d60a381ea7d8d52a1a95f2d0fa
neondoll <bonni_1998@mail.ru>
2024-03-01 20:41:47 +0300

    feat: Interaction between API and user interface: Creation of new transactions (income/expense)

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -8,7 +8,9 @@ class CreateTransactionForm extends AsyncForm {
    * метод renderAccountsList
    * */
   constructor(element) {
-    super(element)
+    super(element);
+
+    this.renderAccountsList();
   }
 
   /**
@@ -16,7 +18,16 @@ class CreateTransactionForm extends AsyncForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
+    Account.list({}, (err, response) => {
+      if (response.success) {
+        const accountsSelectElement = this.element.querySelector('.accounts-select');
+        accountsSelectElement.innerHTML = '';
 
+        response.data.forEach((item) => {
+          accountsSelectElement.insertAdjacentHTML('beforeEnd', `<option value="${item.id}">${item.name}</option>`);
+        });
+      }
+    });
   }
 
   /**
@@ -26,6 +37,21 @@ class CreateTransactionForm extends AsyncForm {
    * в котором находится форма
    * */
   onSubmit(data) {
+    Transaction.create(data, (err, response) => {
+      if (response.success) {
+        this.element.reset();
 
+        switch (this.element.id) {
+          case 'new-income-form':
+            App.getModal('newIncome').close();
+            break;
+          case 'new-expense-form':
+            App.getModal('newExpense').close();
+            break;
+        }
+
+        App.update();
+      }
+    });
   }
 }
--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -11,9 +11,11 @@ class TransactionsWidget {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor( element ) {
-
+  constructor(element) {
+    this.element = element;
+    this.registerEvents();
   }
+
   /**
    * Регистрирует обработчики нажатия на
    * кнопки «Новый доход» и «Новый расход».
@@ -21,6 +23,17 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
-
+    Array.from(this.element.querySelectorAll('.btn')).forEach((btnElement) => {
+      btnElement.addEventListener('click', (event) => {
+        switch (true) {
+          case btnElement.classList.contains('create-income-button'):
+            App.getModal('newIncome').open();
+            break;
+          case btnElement.classList.contains('create-expense-button'):
+            App.getModal('newExpense').open();
+            break;
+        }
+      });
+    });
   }
 }
